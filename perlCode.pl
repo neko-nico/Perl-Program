a7006daa80ed87fd4c7880479f4ec1ed8c3a718f
neko <2015520559@qq.com>
2024-09-25 23:01:19 +0800

    初始化粒子阶段，需要等待distence函数

--- perlCode.pl
+++ perlCode.pl
@@ -2,35 +2,64 @@
 use strict;
 use warnings;
 
-# 示例用法
-my @array1 = (1, 2, 3);
-my @array2 = (4, 5, 6);
-my $sum = add(3, \@array1, \@array2);
-my $dot = dot(3, \@array1, \@array2);
+my $points_Num = 5;
+my $r_equ = 1.5;
+
+my $iniRange = 0.8 * $r_equ * $points_Num **(1/3);
+
+my @points_List = (rand(), rand(), rand(),);
+
+@points_List = map { ($_ * 2 - 1 )* $iniRange / 1.8 } @points_List;
+
+my @random_point = @points_List;
+my $mid_dis
+
+for my $i(1..$points_Num-1){
+    @random_point = (rand(), rand(), rand(),);
+    @random_point = map { ($_ * 2 - 1 )* $iniRange } @random_point;
+    
+
+}
 
 
-print "$sum\n";  # 输出: 5 7 9 = 21
-print "$dot\n";  # 输出: 4 10 18 = 32
 
 
-sub dot {#(length,arry1,arry2)
-    my ($n, $arr1_ref, $arr2_ref) = @_;
+sub norm {
+    my ($array_ref) = @_;
     my $result = 0;
 
-    for (my $i = 0; $i < $n; $i++) {
-        $result = $result + $arr1_ref->[$i] * $arr2_ref->[$i];
+    my @array_f = @$array_ref;
+
+    for my $num (@array_f) {
+        $result += $num**2;
+    }
+
+    return sqrt($result);
+}
+
+sub add {
+    my ($arr1_ref, $arr2_ref) = @_;
+    my $result = 0;
+
+    my @arr1_f = @$arr1_ref;
+    my @arr2_f = @$arr2_ref;
+
+    for my $i (0 .. $#arr1_f) {
+        $result += $arr1_f[$i] + $arr2_f[$i];
     }
 
     return $result;
 }
 
-
-sub add {#(length,arry1,arry2)
-    my ($n, $arr1_ref, $arr2_ref) = @_;
+sub dot {
+    my ($arr1_ref, $arr2_ref) = @_;
     my $result = 0;
 
-    for (my $i = 0; $i < $n; $i++) {
-        $result = $result + $arr1_ref->[$i] + $arr2_ref->[$i];
+    my @arr1_f = @$arr1_ref;
+    my @arr2_f = @$arr2_ref;
+
+    for my $i (0 .. $#arr1_f) {
+        $result += $arr1_f[$i] * $arr2_f[$i];
     }
 
     return $result;
