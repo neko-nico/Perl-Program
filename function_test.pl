6dd28a8681de2a25bbacd53c3ea31eef7264e6fb
neko <2015520559@qq.com>
2024-09-25 23:00:49 +0800

    优化代码结构，添加解引用，提高速度 添加求向量模函数 开始写搜索最近原子以及距离函数

--- function_test.pl
+++ function_test.pl
@@ -4,55 +4,72 @@ use warnings;
 
 
 
-
-
 # 示例用法
-my @array1 = (1, 2, 3);
-my @array2 = (4, 5, 6);
-my $sum = add(3, \@array1, \@array2);
-my $dot = dot(3, \@array1, \@array2);
+my $iniRange = 10;
 
+my @array1 = ($iniRange*rand(), $iniRange*rand(), $iniRange*rand(), $iniRange*rand(),);
+print join(", ", @array1), "\n";
+
+my @array2 = (4, 5, 6, 7);
+print join(", ", @array2), "\n";
+
+my $sum = add(\@array1, \@array2);
+my $dot = dot(\@array1, \@array2);
+my $norm = norm(\@array1);
 
 print "$sum\n";  # 输出: 5 7 9 = 21
 print "$dot\n";  # 输出: 4 10 18 = 32
+print $norm**2,"\n";  # 输出: 1 4 9 = 14
 
 
-sub dot {#(length,arry1,arry2)
-    my ($n, $arr1_ref, $arr2_ref) = @_;
+sub distence{
+    
+
+}
+
+
+sub norm {  # (array)
+    my ($array_ref) = @_;
     my $result = 0;
 
-    for (my $i = 0; $i < $n; $i++) {
-        $result = $result + $arr1_ref->[$i] * $arr2_ref->[$i];
+    my @array_f = @$array_ref;
+
+    for my $num (@array_f) {
+        $result += $num**2;
+    }
+
+    return sqrt($result);
+}
+
+sub add {#(arry1,arry2)
+    my ($arr1_ref, $arr2_ref) = @_;
+    my $result = 0;
+
+    my @arr1_f = @$arr1_ref;
+    my @arr2_f = @$arr2_ref;
+
+    for my $i (0 .. $#arr1_f) {
+        $result += $arr1_f[$i] + $arr2_f[$i];
+    }
+
+    return $result;
+}
+
+sub dot {#(arry1,arry2)
+    my ($arr1_ref, $arr2_ref) = @_;
+    my $result = 0;
+
+    my @arr1_f = @$arr1_ref;
+    my @arr2_f = @$arr2_ref;
+
+    for my $i (0 .. $#arr1_f) {
+        $result += $arr1_f[$i] * $arr2_f[$i];
     }
 
     return $result;
 }
 
 
-sub add {#(length,arry1,arry2)
-    my ($n, $arr1_ref, $arr2_ref) = @_;
-    my $result = 0;
 
-    for (my $i = 0; $i < $n; $i++) {
-        $result = $result + $arr1_ref->[$i] + $arr2_ref->[$i];
-    }
-
-    return $result;
-}
-
-# 定义变量
-my $name = "Alice";
-my $age = 30;
-
-# 定义数组
-my @fruits = ("Apple", "Banana", "Cherry");
-
-# 打印变量
-print "Name: $name\n";
-print "Age: $age\n";
-
-# 打印数组
-print "Fruits: @fruits\n";
-print "Perl version: ", $], "\n";
 
 
