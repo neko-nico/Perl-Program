8a1df99c6f7ea4944b8689616f5cf108336327f9
neko <2015520559@qq.com>
2024-09-26 18:53:48 +0800

    查找最近的原子坐标和距离 相加，相乘等函数内的变量全部加上了_f，防止内外冲突 对原子坐标列表 --> #三个一行进行打印

--- function_test.pl
+++ function_test.pl
@@ -3,36 +3,63 @@ use strict;
 use warnings;
 
 
+my @vector = 0..2;
+# @vector = map { $_ + 0.1} @vector;
 
-# 示例用法
-my $iniRange = 10;
+my @list = 0..2;
 
-my @array1 = ($iniRange*rand(), $iniRange*rand(), $iniRange*rand(), $iniRange*rand(),);
-print join(", ", @array1), "\n";
+print join(", ", @vector), "\n";
+print join(", ", @list), "\n";
 
-my @array2 = (4, 5, 6, 7);
-print join(", ", @array2), "\n";
+my ($dis, $pid) = distence(\@vector,\@list);
+print $dis**2, ', ', $pid,"\n";
 
-my $sum = add(\@array1, \@array2);
-my $dot = dot(\@array1, \@array2);
-my $norm = norm(\@array1);
-
-print "$sum\n";  # 输出: 5 7 9 = 21
-print "$dot\n";  # 输出: 4 10 18 = 32
-print $norm**2,"\n";  # 输出: 1 4 9 = 14
+my $first = (distence(\@vector,\@list))[0];
+print $first**2,"\n";
 
 
-sub distence{
-    
-
+#三个一行进行打印
+for (my $i = 0; $i < @points_List; $i += 3) {
+    print join(" ", @points_List[$i .. $i + 2]), "\n"# if $i + 2 < @points_List;
 }
+
+
+sub distence {#(vector,pList)
+    my ($vector_ref, $pList_ref) = @_;
+    my @vector_f = @$vector_ref;
+    my @pList_f = @$pList_ref;
+
+    my $pointi_f = 0;
+    my @mid_vec = add( \@vector_f, [ @pList_f[0..2] ],1);
+    my $mid_dis = norm(\@mid_vec);
+
+    if ( $mid_dis == 0 ){
+        $pointi_f = 1;
+        @mid_vec = add( \@vector_f, [ @pList_f[3..5] ],1);
+    }
+
+    my $dis = 0;
+
+    for (my $i = 3; $i < $#pList_f; $i += 3){
+        @mid_vec = add( \@vector_f, [ @pList_f[$i..$i+2] ],1);
+        $dis = norm(\@mid_vec);
+
+        if ($mid_dis > $dis && $dis != 0) {
+            $mid_dis = $dis;
+            $pointi_f = $i/3;
+        }
+    }
+
+    return ($mid_dis, $pointi_f);  # 返回多个值
+}
+
 
 
 sub norm {  # (array)
     my ($array_ref) = @_;
-    my $result = 0;
-
     my @array_f = @$array_ref;
+
+    my $result = 0;
 
     for my $num (@array_f) {
         $result += $num**2;
@@ -41,26 +68,26 @@ sub norm {  # (array)
     return sqrt($result);
 }
 
-sub add {#(arry1,arry2)
-    my ($arr1_ref, $arr2_ref) = @_;
-    my $result = 0;
-
+sub add {
+    my ($arr1_ref, $arr2_ref,$control_f) = @_;
     my @arr1_f = @$arr1_ref;
     my @arr2_f = @$arr2_ref;
 
+    my @result;
+
     for my $i (0 .. $#arr1_f) {
-        $result += $arr1_f[$i] + $arr2_f[$i];
+        $result[$i] = $arr1_f[$i] + $arr2_f[$i]* (-1)**$control_f;
     }
 
-    return $result;
+    return @result;
 }
 
 sub dot {#(arry1,arry2)
     my ($arr1_ref, $arr2_ref) = @_;
-    my $result = 0;
-
     my @arr1_f = @$arr1_ref;
     my @arr2_f = @$arr2_ref;
+
+    my $result = 0;
 
     for my $i (0 .. $#arr1_f) {
         $result += $arr1_f[$i] * $arr2_f[$i];
--- storeHouse.pl
+++ storeHouse.pl
@@ -1,6 +1,26 @@
 #不再需要的样例代码
 
 
+
+###数组相加 相乘 模长
+# 示例用法
+my $iniRange = 10;
+
+my @array1 = ($iniRange*rand(), $iniRange*rand(), $iniRange*rand(), $iniRange*rand(),);
+print join(", ", @array1), "\n";
+
+my @array2 = (4, 5, 6, 7);
+print join(", ", @array2), "\n";
+
+my @sum = add(\@array1, \@array2,1);
+my $dot = dot(\@array1, \@array2);
+my $norm = norm(\@array1);
+
+print "@sum\n";  # 输出: 5 7 9 = 21
+print "$dot\n";  # 输出: 4 10 18 = 32
+print $norm**2,"\n";  # 输出: 1 4 9 = 14
+
+
 #实现数组中所有元素乘以一个数
 my @array1 = (rand(), rand(), rand());  # 生成三个随机数
 my $multiplier = 10;
